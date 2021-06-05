4d9c7ca0faf241f520ae7ccc05f7f3e9355d1b6e
Dan Rice <dan.rice.92@outlook.com>
2021-06-05 14:33:27 -0600

    div instead of article because wat

--- tests/integration/components/job-test.js
+++ tests/integration/components/job-test.js
@@ -9,13 +9,13 @@ module('Integration | Component | job', function(hooks) {
   test('it renders information about the job', async function(assert) {
     await render(hbs`<Job />`);
 
-    assert.dom('article').hasClass('job');
-    assert.dom('article h3').hasText('Toys R Us');
-    assert.dom('article .detail.date-range').includesText('Start Date: November 2008');
-    assert.dom('article .detail.date-range').includesText('End Date: January 2009');
-    assert.dom('article .detail.pay-range').includesText('Starting Pay: $8/hour');
-    assert.dom('article .detail.pay-range').includesText('Ending Pay: $8/hour');
-    assert.dom('article .detail.location').includesText('Fort Collins, CO');
-    assert.dom('article .detail.career').includesText('Part of my career in: Retail Sales');
+    assert.dom('div').hasClass('job');
+    assert.dom('div h3').hasText('Toys R Us');
+    assert.dom('div .detail.date-range').includesText('Start Date: November 2008');
+    assert.dom('div .detail.date-range').includesText('End Date: January 2009');
+    assert.dom('div .detail.pay-range').includesText('Starting Pay: $8/hour');
+    assert.dom('div .detail.pay-range').includesText('Ending Pay: $8/hour');
+    assert.dom('div .detail.location').includesText('Fort Collins, CO');
+    assert.dom('div .detail.career').includesText('Part of my career in: Retail Sales');
   });
 });
