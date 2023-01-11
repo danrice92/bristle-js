336256063f0807c2840992779622ffe6fb139bb8
Dan Rice <dan.rice.92@outlook.com>
2023-01-10 21:45:59 -0700

    verification fields management

--- app/components/email-verification-input.js
+++ app/components/email-verification-input.js
@@ -1,11 +1,39 @@
 import Component from '@glimmer/component';
-// import { action } from '@ember/object';
-// import bootstrap from 'bootstrap';
+import { action } from '@ember/object';
+import _ from 'lodash';
 
 export default class EmailVerificationInput extends Component {
   fields = [1, 2, 3, 4, 5, 6];
-  // @action collapseMobileDropdown() {
-  //   const navBar = document.querySelector('.navbar-collapse');
-  //   new bootstrap.Collapse(navBar, { toggle: false }).hide();
-  // }
+
+  fieldAtPosition = (position) =>
+    document.querySelector(`input[data-position="${position}"]`);
+
+  @action moveCursor(event) {
+    const { key, metaKey, target } = event;
+    if (!event.metaKey) event.preventDefault();
+    const initialValue = target.value;
+    const position = _.toNumber(target.dataset.position);
+
+    switch (key) {
+      case 'Backspace':
+        if (_.isEmpty(initialValue) && position > 1) {
+          const previousField = this.fieldAtPosition(position - 1);
+          previousField.value = '';
+          previousField.focus();
+        } else {
+          target.value = '';
+        }
+        break;
+      case 'Delete':
+        target.value = '';
+        break;
+      default:
+        if (metaKey || key.length !== 1) return;
+
+        target.value = _.capitalize(_.toString(key));
+        if (position < 6) {
+          this.fieldAtPosition(position + 1).focus();
+        }
+    }
+  }
 }
